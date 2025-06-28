5cec581d0ce8990e0b2264ff56042aa4c9945e48
Anurag Singh <anuragsingh4932@gmail.com>
2025-06-28 13:18:58 +0530

    init

---
+++ app/(root)/page.jsx
@@ -0,0 +1,10 @@
+import LeftSidebar from "@/components/LeftSidebar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <LeftSidebar />
+    </div>
+  );
+}
